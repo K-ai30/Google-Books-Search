f00470bb1737f199c2e5a3cf313d23658d157ca2
K-ai30 <artpirate11@aim.com>
2020-08-31 23:49:52 -0400

    added script seed file and used prettier

--- models/books.js
+++ models/books.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    title: { type: String, required: true },
-    author: [{ type: String, required: true }],
-    description: { type: String },
-    image: { type: String, trim: true },
-    link: { type: String, trim: true },
-    date: { type: Date, default: Date.now },
+  title: { type: String, required: true },
+  author: [{ type: String, required: true }],
+  description: { type: String },
+  image: { type: String, trim: true },
+  link: { type: String, trim: true },
+  date: { type: Date, default: Date.now },
 });
 
 const Book = mongoose.model("Book", bookSchema);
